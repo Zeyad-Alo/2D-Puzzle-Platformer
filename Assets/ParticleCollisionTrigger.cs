7545e08ebf6eb96562ba901b13bf5e85a0b4dc9d
Zeyad-Alo <zeyadmansour2@gmail.com>
2022-07-20 18:32:37 +0200

    In progress

---
+++ Assets/ParticleCollisionTrigger.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ParticleCollisionTrigger : MonoBehaviour
+{
+    public GameObject walltrigger;
+    float health = 5f;
+    bool active = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject obj)
+    {
+        if (obj.tag == "ParticleTriggered")
+        {
+            health -= 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            Toggle();
+        }
+    }
+
+    void Toggle()
+    {
+        health = 5;
+        active = !active;
+        if (active)
+        {
+            Color tempcolor = new Color(0.764151f,0,0,1);
+            walltrigger.GetComponent<Tilemap> ().color = tempcolor;
+            walltrigger.layer = LayerMask.NameToLayer("Closed");
+        }
+        else
+        {
+            Color tempcolor = new Color(0.764151f,0,0,0.2745098f);
+            walltrigger.GetComponent<Tilemap> ().color = tempcolor;
+            walltrigger.layer = LayerMask.NameToLayer("Open");
+        }
+    }
+}
---
+++ Assets/ParticleGun.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleGun : MonoBehaviour
+{
+
+    private ParticleSystem _particleSystem;
+    private ParticleSystem.EmissionModule emission;
+
+    Vector3 mousePos;
+
+
+    void OnEnable () {
+        _particleSystem = gameObject.GetComponent<ParticleSystem>();
+        emission = _particleSystem.emission;
+        emission.enabled = false;
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButton(0))
+        {
+            emission.enabled = true;
+        }
+        else {
+            emission.enabled = false;
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = mousePos - gameObject.transform.position;
+        //gameObject.transform.LookAt(gameObject.transform.position + direction);
+        //Debug.Log(direction);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        var Shape = _particleSystem.shape;
+        Shape.rotation = new Vector3 (0,0,angle);
+    }
+}
